e661440c6accceefe2487304e03644cc2a0b4907
tjheckler <timothyheckler@gmail.com>
2018-05-03 09:44:07 -0500

    DateStuff

--- test/com/arkansascodingacademy/DateTimeUtilTest.java
+++ test/com/arkansascodingacademy/DateTimeUtilTest.java
@@ -2,6 +2,8 @@ package com.arkansascodingacademy;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DateTimeUtilTest
@@ -10,6 +12,11 @@ class DateTimeUtilTest
     @Test
     void isHappyHour()
     {
+
+        LocalDateTime fridayNotHappyHour = LocalDateTime.of(2018,5, 4, 8,0);
+        LocalDateTime fridayHappyHour = LocalDateTime.of(2018,5, 4, 17,0);
+        assertFalse(DateTimeUtil.isHappyHour(fridayNotHappyHour));
+        assertTrue(DateTimeUtil.isHappyHour(fridayHappyHour));
 
     }
 }
